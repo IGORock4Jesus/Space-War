44b1f2828b0304981048acfc27806b91b292e28f
Igor Kozlov <IGORock.guitar@outlook.com>
2018-08-23 17:07:17 +0600

    Добавлен сброс устройства.

--- Space War Managed/Renderer.cs
+++ Space War Managed/Renderer.cs
@@ -2,8 +2,10 @@
 using SharpDX.Direct3D9;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Space_War_Managed
@@ -12,6 +14,8 @@ namespace Space_War_Managed
 	{
 		Direct3D direct;
 		Device device;
+		bool canDraw;
+		private bool inDrawing;
 
 		public delegate void DrawHandler(Device device);
 		public event DrawHandler Drawing;
@@ -24,7 +28,6 @@ namespace Space_War_Managed
 
 			device = new Device(direct, 0, DeviceType.Hardware, form.Handle, CreateFlags.HardwareVertexProcessing, new PresentParameters
 			{
-				BackBufferCount = 1,
 				BackBufferFormat = Format.X8R8G8B8,
 				BackBufferHeight = form.ClientSize.Height,
 				BackBufferWidth = form.ClientSize.Width,
@@ -34,17 +37,71 @@ namespace Space_War_Managed
 			});
 
 			device.SetRenderState(RenderState.Lighting, false);
+
+			form.SizeChanged += Form_SizeChanged;
+
+			canDraw = true;
+		}
+
+		private void Form_SizeChanged(object sender, EventArgs e)
+		{
+			if (!(sender is MainForm mainForm)) return;
+
+			var size = mainForm.ClientSize;
+
+			OnPreReset();
+
+			device.Reset(new PresentParameters
+			{
+				BackBufferFormat = Format.X8R8G8B8,
+				BackBufferHeight = size.Height,
+				BackBufferWidth = size.Width,
+				DeviceWindowHandle = mainForm.Handle,
+				SwapEffect = SwapEffect.Discard,
+				Windowed = true
+			});
+
+			OnPostReset();
+		}
+
+		private void OnPostReset()
+		{
+			canDraw = true;
+		}
+
+		private void OnPreReset()
+		{
+			canDraw = false;
+			while (inDrawing)
+			{
+				Thread.Sleep(1);
+			}
 		}
 
 		public void Rendering()
 		{
-			device.Clear(ClearFlags.Target, Color.Gray, 1.0f, 0);
-			device.BeginScene();
+			if (!canDraw) return;
 
-			Drawing?.Invoke(device);
+			inDrawing = true;
 
-			device.EndScene();
-			device.Present();
+			try
+			{
+				device.Clear(ClearFlags.Target, Color.Gray, 1.0f, 0);
+				device.BeginScene();
+
+				Drawing?.Invoke(device);
+
+				device.EndScene();
+				device.Present();
+			}
+			catch(Exception ex)
+			{
+				Debug.WriteLine(ex);
+				// ждем 1 сек
+				Thread.Sleep(1000);
+			}
+
+			inDrawing = false;
 		}
 
 		public void Dispose()
